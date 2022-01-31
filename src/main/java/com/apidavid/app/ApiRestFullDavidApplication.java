d0849a6c9251cd60528ac5fc54249fe478eafcf2
DavidOrdonezSgby <david.ordonez.est@tecazuay.edu.ec>
2022-01-31 16:52:39 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/apidavid/app/ApiRestFullDavidApplication.java
@@ -0,0 +1,13 @@
+package com.apidavid.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestFullDavidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestFullDavidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apidavid/app/ApiRestFullDavidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apidavid.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestFullDavidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
